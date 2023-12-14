912792eb9affa3513458a88c96586f9631457fb9
Alexandr Zaycev <hunter290592@yandex.ru>
2023-12-14 12:21:23 +0300

    add types for webpack config, decomposition of webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,31 +1,22 @@
-const path = require('path');
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+import path from 'path';
+import { Configuration } from 'webpack';
 
-module.exports = {
-  mode: 'development',
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildPaths } from './config/build/types/config';
+
+const paths: BuildPaths = {
   entry: path.resolve(__dirname, 'src', 'index.ts'),
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
+  build: path.resolve(__dirname, 'build'),
+  html: path.resolve(__dirname, 'public', 'index.html'),
 };
+
+const mode = 'development';
+const isDev = mode === 'development';
+
+const config: Configuration = buildWebpackConfig({
+  mode: 'development',
+  paths,
+  isDev,
+});
+
+export default config;
